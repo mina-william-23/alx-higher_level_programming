8b1bab8c0c6af385f2c720b0582c49ba2cdb41d5
mina-william-23 <capeandroid50@gmail.com>
2023-06-06 05:13:31 -0700

    add 1-last_digit.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_dig = number % 10 if number >= 0 else number % -10
+message  = ''
+if (last_dig == 0):
+    message = 'and is 0'
+elif (last_dig < 6):
+    message = 'and is less than 6 and not 0'
+else:
+    message = 'and is less than 6 and not 0'
+print(f'Last digit of {number:d} is {last_dig:d} {message:s}')
