7452f53b292fab2efe3ccb53e2d6afba232567a0
mina-william-23 <capeandroid50@gmail.com>
2023-06-06 07:14:26 -0700

    add 9-print_last-digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def uprint_last_digit(number):
+def print_last_digit(number):
     if (number < 0):
-        number *= -1
+        number = -number
     last_dig = number % 10
     print('{:d}'.format(last_dig), end='')
     return last_dig
