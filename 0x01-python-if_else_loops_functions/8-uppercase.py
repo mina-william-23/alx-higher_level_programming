91cb0fef112424614dd283d14e672a3d127e1b7f
mina-william-23 <capeandroid50@gmail.com>
2023-06-06 06:52:15 -0700

    add 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    char = ''
+    for i in str:
+        char = i
+        if (ord(i) >= ord('a') and ord(i) <= ord('z')):
+            char = chr(ord(i) - ord('a') + ord('A')
+        print(char, end='')
+    print('')
