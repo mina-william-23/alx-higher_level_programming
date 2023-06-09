7410e7e4aab36111420806c75854ea6284f38b7d
mina-william-23 <capeandroid50@gmail.com>
2023-06-09 13:23:38 -0700

    add task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - check if linked list is palindrom
+ * @head: linked list
+ * Return: 0 if not palindrome 1 is palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	size_t len_list = 0;
+	listint_t *current = *head;
+	int i = 0, j = 0, *arr;
+
+	while (current)
+	{
+		len_list++;
+		current = current->next;
+	}
+	if (len_list <= 1)
+		return (1);
+
+	arr = malloc(sizeof(int) * ((int)len_list / 2));
+	current = *head;
+	while (current)
+	{
+		if (i < len_list / 2)
+			arr[j++] = current->n;
+		else if (i == len_list / 2 && len_list % 2)
+		{
+			current = current->next;
+			i++;
+			continue;
+		}
+		else
+		{
+			if (arr[--j] != current->n)
+			{
+				free(arr);
+				return (0);
+			}
+		}
+		i++;
+		current = current->next;
+
+	}
+	free(arr);
+	return (1);
+}
