c4ec83b541002ede9693ddb2d88aa650c778f699
mina-william-23 <capeandroid50@gmail.com>
2023-06-10 02:26:45 -0700

    add task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == None or len(my_list) == 0:
+    if my_list is None or len(my_list) == 0:
         return None
     mx = my_list[0]
     for x in my_list:
