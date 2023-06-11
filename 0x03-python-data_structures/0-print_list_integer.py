21cf0611c8a4e0585626584802d17085a796f7f9
mina-william-23 <capeandroid50@gmail.com>
2023-06-11 16:49:31 -0700

    edit Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print('{:d}'.format(i))
+    # check if not None aas in will throw error of iterate on noniterable
+    if my_list:
+        for i in my_list:
+            print('{:d}'.format(i))
