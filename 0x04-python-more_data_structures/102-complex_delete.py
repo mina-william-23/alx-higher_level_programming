b56917fda13f85cf9fb3f329a7184e30ebe328f9
mina-william-23 <capeandroid50@gmail.com>
2023-06-14 11:37:10 -0700

    add versions of task 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,3 +4,14 @@ def complex_delete(a_dictionary, value):
         if a_dictionary[key] == value:
             del a_dictionary[key]
     return a_dictionary
+# def complex_delete(a_dictionary, value):
+#     for k, v in filter(lambda p: p[1] == value, list(a_dictionary.items())):
+#             del a_dictionary[k]
+#     return a_dictionary
+
+# def complex_delete(a_dictionary, value):
+# keys=map(lambda x:x[0] if x[1]==value else None, list(a_dictionary.items()))
+# for k in keys:
+#         if k is not None:
+#             del a_dictionary[k]
+#     return a_dictionary
