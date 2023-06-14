3407d3498bcabf4c224db0138c95be0c5cd641af
mina-william-23 <capeandroid50@gmail.com>
2023-06-14 05:23:25 -0700

    edit task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
+    if my_list is not None:
         return [i if i != search else replace for i in my_list]
     return None
