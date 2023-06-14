828a31fca7bff2466733b028b780f271d56f3849
mina-william-23 <capeandroid50@gmail.com>
2023-06-14 05:30:06 -0700

    edit task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    # dictionary is none or empty return none
+    if not a_dictionary:
         return None
     mx = max(a_dictionary.values())
     for key, value in a_dictionary.items():
         if mx == value:
             return key
-    return None
