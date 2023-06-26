ea075dfa754a91240dfb11a44b0b8ff73d45ee5e
mina-william-23 <capeandroid50@gmail.com>
2023-06-26 11:27:36 -0700

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     try:
         print('{:d}'.format(value))
         return True
-    /*there are two types of errors can occurs here TypeError, ValueError*/
     except Exception:
         return False
