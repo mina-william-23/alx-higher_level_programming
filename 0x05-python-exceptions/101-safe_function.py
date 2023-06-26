eff7af4e67d1597fdd8b9d92cf34647adb752c99
mina-william-23 <capeandroid50@gmail.com>
2023-06-26 13:46:17 -0700

    task 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        ret = fct(args)
+        return ret
+    except Exception as err:
+        print('Exception: {}'.format(err), file=sys.stderr)
+        return None
