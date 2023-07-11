92168a8300fb691fc49edf3a98f9fb3370eb98f5
mina-william-23 <capeandroid50@gmail.com>
2023-07-10 19:37:52 -0700

    add 101 task

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@
 
 def add_attribute(ob, key, value):
     ''' set attribute ob.key = value in ob object '''
-    # if not hasattr(ob, '__dict__'):
-    if hasattr(ob, '__slots__'):
+    if not hasattr(ob, '__dict__'):
+    # if hasattr(ob, '__slots__'):
         raise TypeError('can\'t add new attribute')
     setattr(ob, key, value)
