07ae770d763737ff302efb2b8f7fcb4b9e80ac92
mina-william-23 <capeandroid50@gmail.com>
2023-07-11 17:12:12 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,9 +3,9 @@
 import sys
 
 
-if __name__ = "__main__":
-    save_t_json_f = __import__('5-save_to_json_file.py').save_to_json_file
-    load_fm_json_f = __import__('6-load_from_json_file.py').load_from_json_file
+if __name__ == "__main__":
+    save_t_json_f = __import__('5-save_to_json_file').save_to_json_file
+    load_fm_json_f = __import__('6-load_from_json_file').load_from_json_file
 
     try:
         ob = load_fm_json_f('add_item.json')
