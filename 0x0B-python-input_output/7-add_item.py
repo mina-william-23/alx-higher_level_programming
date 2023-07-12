2e8d2b807f898cb802a69e33325c3188d139cd8f
mina-william-23 <capeandroid50@gmail.com>
2023-07-11 17:07:39 -0700

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' 7-add_item.py module '''
+import sys
+
+
+if __name__ = "__main__":
+    save_t_json_f = __import__('5-save_to_json_file.py').save_to_json_file
+    load_fm_json_f = __import__('6-load_from_json_file.py').load_from_json_file
+
+    try:
+        ob = load_fm_json_f('add_item.json')
+    except Exception:
+        ob = []
+    ob.extend(sys.argv[1:])
+    save_t_json_f(ob, 'add_item.json')
