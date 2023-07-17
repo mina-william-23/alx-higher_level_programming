eed016bbfaf3f5b206ecff7329bf0fcdb8c30431
mina-william-23 <capeandroid50@gmail.com>
2023-07-17 14:23:03 -0700

    edit tests

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -532,6 +532,3 @@ class TestBase_load_from_file_csv(unittest.TestCase):
     def test_load_from_file_csv_more_than_one_arg(self):
         with self.assertRaises(TypeError):
             Base.load_from_file_csv([], 1)
-
-if __name__ == "__main__":
-    unittest.main()
