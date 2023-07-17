1d744256b2b72a53484f3065f152c7b24a0652b1
mina-william-23 <capeandroid50@gmail.com>
2023-07-17 11:59:04 -0700

    edit tests

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import unittest
+import sys
+import io
 from models.base import Base
 from models.rectangle import Rectangle
 
@@ -108,4 +110,107 @@ class TestRectangle(unittest.TestCase):
         self.assertRaises(ValueError, Rectangle, 2, 1, 1, -1)
 
     def test_rectangle_update(self):
-        pass
+        r1 = Rectangle(1,2)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.id, 1)
+        r1.update()
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        r1.update(10)
+        self.assertEqual(r1.id, 10)
+        r1.update(20, 30)
+        self.assertEqual(r1.id, 20)
+        self.assertEqual(r1.width, 30)
+        r1.update(30, 40, 50)
+        self.assertEqual(r1.id, 30)
+        self.assertEqual(r1.width, 40)
+        self.assertEqual(r1.height, 50)
+        r1.update(40, 50, 60, 70)
+        self.assertEqual(r1.id, 40)
+        self.assertEqual(r1.width, 50)
+        self.assertEqual(r1.height, 60)
+        self.assertEqual(r1.x, 70)
+        r1.update(50, 60, 70, 80, 90)
+        self.assertEqual(r1.id, 50)
+        self.assertEqual(r1.width, 60)
+        self.assertEqual(r1.height, 70)
+        self.assertEqual(r1.x, 80)
+        self.assertEqual(r1.y, 90)
+        r1.update(60, 70, 80, 90, 100, id = 10, width = 20)
+        self.assertEqual(r1.id, 60)
+        self.assertEqual(r1.width, 70)
+        self.assertEqual(r1.height, 80)
+        self.assertEqual(r1.x, 90)
+        self.assertEqual(r1.y, 100)
+        r1.update(70, id = 10, width = 20)
+        self.assertNotEqual(r1.id, 10)
+        self.assertEqual(r1.id, 70)
+        self.assertNotEqual(r1.width, 20)
+        r1.update(id = 10, width = 20)
+        self.assertEqual(r1.id, 10)
+        self.assertNotEqual(r1.id, 70)
+        self.assertEqual(r1.width, 20)
+        self.assertNotEqual(r1.width, 70)
+        r1.update(hight = 30)
+        self.assertEqual(r1.height, 80)
+        self.assertNotEqual(r1.height, 30)
+        r1.update(x = 5, y = 6, id = 100, width = 200, height = 400)
+        self.assertEqual(r1.id, 100)
+        self.assertEqual(r1.width, 200)
+        self.assertEqual(r1.height, 400)
+        self.assertEqual(r1.x, 5)
+        self.assertEqual(r1.y, 6)
+        r1.update(x = 5, xx=8, y = 6, id = 100, width = 200, height = 400)
+        self.assertEqual(r1.id, 100)
+        self.assertEqual(r1.width, 200)
+        self.assertEqual(r1.height, 400)
+        self.assertEqual(r1.x, 5)
+        self.assertNotEqual(r1.x, 8)
+        self.assertEqual(r1.y, 6)
+
+    def test_rectangle_area(self):
+        r1 = Rectangle(10,20)
+        self.assertEqual(r1.area(), 200)
+        r1.update(1, 100, 20, 5, 6)
+        self.assertEqual(r1.area(), 2000)
+        r1.width = 5
+        r1.height = 6
+        self.assertEqual(r1.area(), 30)
+
+    @staticmethod
+    def capture_our_buffer(obj, method):
+        """Captures and returns text printed to stdout.
+
+        Args:
+            rect (Rectangle): The Rectangle to print to stdout.
+            method (str): The method to run on rect.
+        Returns:
+            The text printed to stdout by calling method on sq.
+        """
+        buffer = io.StringIO()
+        sys.stdout = buffer
+        if method == "print":
+            print(obj)
+        elif method == "display":
+            obj.display()
+        sys.stdout = sys.__stdout__
+        return buffer
+
+    def test_rectangle_display_width_height(self):
+        r1 = Rectangle(2, 3)
+        buffer = TestRectangle.capture_our_buffer(r1, "display")
+        self.assertEqual("##\n##\n##\n", buffer.getvalue())
+
+    def test_rectangle_display_width_height_x(self):
+        r1 = Rectangle(2, 3, 2)
+        buffer = TestRectangle.capture_our_buffer(r1, "display")
+        self.assertEqual("  ##\n  ##\n  ##\n", buffer.getvalue())
+
+    def test_rectangle_display_width_height_x_y(self):
+        r1 = Rectangle(2, 3, 2, 4)
+        buffer = TestRectangle.capture_our_buffer(r1, "display")
+        self.assertEqual("\n\n\n\n  ##\n  ##\n  ##\n", buffer.getvalue())
