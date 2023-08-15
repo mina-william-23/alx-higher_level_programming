e404e65371dbf95375024ee43e1a59e24591206e
mina-william-23 <capeandroid50@gmail.com>
2023-08-15 14:37:04 -0700

    task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -14,7 +14,7 @@
 -- id = 4, name = “George”, score = 8
 CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
-	name VARCHAR(256)
+	name VARCHAR(256),
 	score INT
 	);
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
