da8379e88cc4d70ef25dfa60f389f489e0e7a6af
mina-william-23 <capeandroid50@gmail.com>
2023-08-16 13:13:34 -0700

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,7 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+-- This script creates the database hbtn_0d_2 and the user user_0d_2 (with SELECT privilege)
+-- password is user_0d_2_pwd
+-- it does not fail if hbtn_0d_2 or user_0d_2 already exists
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
