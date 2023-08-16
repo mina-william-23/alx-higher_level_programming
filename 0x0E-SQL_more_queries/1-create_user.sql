e17dcc8305c317897d337b1dafa89d6667ae7831
mina-william-23 <capeandroid50@gmail.com>
2023-08-16 13:01:59 -0700

    task 0

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
--- creates the MySQL server user user_0d_1
+-- This script creates the MySQL server user user_0d_1 (with all privileges)
+-- password is user_0d_1_pwd
+-- it does not fail if user_0d_1 already exists
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
