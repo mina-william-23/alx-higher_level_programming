cfb7616de36a69d5191adc510ac2d2f3ab2e3d67
mina-william-23 <capeandroid50@gmail.com>
2023-08-16 13:19:27 -0700

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- comment head for task
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL FOR *.* TO user_0d_1@localhost;
+GRANT ALL ON *.* TO user_0d_1@localhost;
