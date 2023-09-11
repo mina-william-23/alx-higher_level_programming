ee936e281962005c86628e6a0dbac3d56249cf01
mina-william-23 <capeandroid50@gmail.com>
2023-09-11 12:56:08 -0700

    tasks 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-output = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const output = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 output.forEach((item) => console.log(item));
