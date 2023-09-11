02133407c001ec00b83a745581e4e288caf5157e
mina-william-23 <capeandroid50@gmail.com>
2023-09-11 13:38:27 -0700

    tasks 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 let agv = process.argv[2];
-if (typeof agv !== 'undefined') {
-  if (isNaN(agv)) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let i = 0; i < agv; i++) {
-      console.log('C is fun');
-    }
+agv = Number(agv);
+if (isNaN(agv)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < agv; i++) {
+    console.log('C is fun');
   }
 }
+
