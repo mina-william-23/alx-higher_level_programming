f88ed529552d3b1e6e4be081ec60714fc3ce326e
mina-william-23 <capeandroid50@gmail.com>
2023-09-11 17:05:09 -0700

    simple version of task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-const n1 = process.argv[2];
-const n2 = process.argv[3];
-if (isNaN(n1) || isNaN(n2)) {
-  console.log('NaN');
-} else {
-  console.log(`${Number(n1) + Number(n2)}`);
-}
+console.log(Number(process.argv[2]) + Number(process.argv[3]));
