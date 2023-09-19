ad5719ab9b07cd0b54e3cb50a9160ffd11227545
mina-william-23 <capeandroid50@gmail.com>
2023-09-18 21:58:25 -0700

    task 100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python3
-"""
-State module
-"""
+''' model_state_fetch_all '''
 
 
-from sqlalchemy.orm import sessionmaker
+from sys import argv
 from sqlalchemy import create_engine
-from relationship_state import Base, State
-from relationship_city import City
-import sys
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import Base, City
 
-
-def create_state_cities():
-    """Fetches all states"""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
+if __name__ == "__main__":
+    usr = argv[1]
+    paswd = argv[2]
+    dt = argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        username, password, database), pool_pre_ping=True)
-
+        usr, paswd, dt, pool_pre_ping=True))
     Base.metadata.create_all(engine)
-    Session = sessionmaker()
-    Session.configure(bind=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
+    # one way to use cities list in states table
+    # new_state = State(name='California')
+    # new_state.cities.append(City(name='San Francisco'))
+    # session.add(new_state)
+
+    # another way to use city backref attribute state
+    # which is set in relationship in State class
+    # session.add(City(name="San Francisco", state=State(name="California")))
     new_state = State(name='California')
     new_city = City(name='San Francisco')
     new_state.cities.append(new_city)
     session.add(new_state)
     session.add(new_city)
     session.commit()
-    session.close()
-
-
-if __name__ == "__main__":
-    create_state_cities()
+    session.close() 
