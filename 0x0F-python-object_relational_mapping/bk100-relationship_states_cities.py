b483d035d66fe9f7decc5d37e78a72a681de3563
mina-william-23 <capeandroid50@gmail.com>
2023-09-18 21:48:37 -0700

    task 100

---
+++ 0x0F-python-object_relational_mapping/bk100-relationship_states_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' model_state_fetch_all '''
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import Base, City
+
+if __name__ == "__main__":
+    usr = argv[1]
+    paswd = argv[2]
+    dt = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        usr, paswd, dt, pool_pre_ping=True))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # one way to use cities list in states table
+    new_state = State(name='California')
+    new_state.cities.append(City(name='San Francisco'))
+    session.add(new_state)
+
+    # another way to use city backref attribute state
+    # which is set in relationship in State class
+    # session.add(City(name="San Francisco", state=State(name="California")))   
+    session.commit()
+    session.close()
