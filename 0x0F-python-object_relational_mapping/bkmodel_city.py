c7ccc7a2e466dbcad49416323f0e86661ca2f71c
mina-william-23 <capeandroid50@gmail.com>
2023-09-18 21:28:40 -0700

    task 100

--- 0x0F-python-object_relational_mapping/bkmodel_city.py
+++ 0x0F-python-object_relational_mapping/bkmodel_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' model City module '''
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -18,4 +18,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, nullable=False, Foreign_key=('states.id'))
