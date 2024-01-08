c4608ac807ea10b95d2f1060283904d7b948ed5d
mina-william <capeandroid50@gmail.com>
2024-01-08 19:57:34 +0200

    first commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const inputs = process.argv.length;
+if (inputs === 2) {
+  console.log('No argument');
+} else if (inputs === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
