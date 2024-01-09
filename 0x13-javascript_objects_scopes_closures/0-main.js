0c10df63d4ab11f75bcd9d85f8811d733735ac6f
mina-william-23 <capeandroid50@gmail.com>
2024-01-09 17:27:51 +0200

    7

--- 0x13-javascript_objects_scopes_closures/0-main.js
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./1-rectangle');
+const nbOccurences = require('./7-occurrences').nbOccurences;
 
-const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
-
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
+console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
+console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
+console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
