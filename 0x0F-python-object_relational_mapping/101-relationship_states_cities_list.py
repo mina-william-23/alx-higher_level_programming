23610d7cfe4db7fe6190fa3cf597daf9390793d1
mina-william-23 <capeandroid50@gmail.com>
2024-01-14 02:34:05 +0200

    102

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -2,6 +2,7 @@
 """lists all State objects, and corresponding City objects,
 contained in the database hbtn_0e_101_usa"""
 
+
 if __name__ == "__main__":
 
     import sys
@@ -9,7 +10,6 @@ if __name__ == "__main__":
     from relationship_city import City
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
