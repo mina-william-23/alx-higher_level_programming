a7cb6b769ac6831229cc027a9b86973a3b04b64d
mina-william-23 <capeandroid50@gmail.com>
2024-01-14 20:13:53 +0200

    edit relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -6,8 +6,8 @@
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
-#from relationship_city import City
+from relationship_state import Base, State
+from relationship_city import City
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -23,6 +23,4 @@ class City(Base):
                 nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship('State',
-                         back_populates='cities',
-                         cascade='all, delete')
+    #state = relationship('State', back_populates='cities', cascade='all, delete')
