4b04204f2c6889a0a61935b53a7bd1633cdd9ffe
mina-william-23 <capeandroid50@gmail.com>
2024-01-26 00:55:47 +0200

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# curl -sI "$1" | grep Content-Length | cut -d' ' -f2
+#  displays the size of the body - curl -sI "$1" | grep Content-Length | cut -d' ' -f2
 curl -s -o /dev/null -w "%{size_download}\n" "$1"
