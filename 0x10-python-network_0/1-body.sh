bebf4c9d921a496d0c449fcee7f87ea1d3a44230
mina-william-23 <capeandroid50@gmail.com>
2024-01-26 01:22:52 +0200

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#  displays the size of the body - curl -sI "$1" | grep Content-Length | cut -d' ' -f2
-status_code=$(curl -sL -w "%{http_code}" -o /dev/null "$1")
-if [ "$status_code" -eq 200 ]; then
-    curl -sL "$1"
-fi
+# status_code=$(curl -sL -w "%{http_code}" -o /dev/null "$1")--- if [ "$status_code" -eq 200 ]; then curl -sL "$1" fi
+curl -sL "$1"
