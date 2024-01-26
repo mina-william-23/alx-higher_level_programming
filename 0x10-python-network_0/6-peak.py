2cb841fce05a790fcdd4018e8c6f3ce7d886fb90
mina-william-23 <capeandroid50@gmail.com>
2024-01-26 02:25:35 +0200

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!usr/bin/python3
 """Module for finding a peak in a list of unsorted integers."""
+
+
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
     if list_of_integers == []:
@@ -7,6 +9,7 @@ def find_peak(list_of_integers):
     before_element = list_of_integers[0]
     for i in range(len(list_of_integers)):
         if list_of_integers[i] >= before_element:
-            if i == len(list_of_integers) - 1 or list_of_integers[i] >= list_of_integers[i + 1]:
+            if (i == len(list_of_integers) - 1 or
+                    list_of_integers[i] >= list_of_integers[i + 1]):
                 return list_of_integers[i]
         before_element = list_of_integers[i]
