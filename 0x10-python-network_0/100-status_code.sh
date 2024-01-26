af4baf147e4efeee311e5ad26c1235bfa87e9670
mina-william-23 <capeandroid50@gmail.com>
2024-01-26 02:50:35 +0200

    task 100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI -w "%{http_code}\n" -o /dev/null "$1"
+# Sends a request to a URL, displays only the status code of the response.
+curl -sI -o /dev/null  -w "%{http_code}" "$1"
