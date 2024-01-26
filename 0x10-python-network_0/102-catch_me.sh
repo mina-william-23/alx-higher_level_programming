a2788acb5bbf26289a6389400de21a0afa605ffc
mina-william-23 <capeandroid50@gmail.com>
2024-01-26 04:34:35 +0200

    task 101-102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL, displays only the status code of the response.
-curl -s -o /dev/null -w 'You got me!\n' "http://0.0.0.0:5000/catch_me"
+curl -s -o /dev/null -w 'You got me!' "http://0.0.0.0:5000/catch_me"
