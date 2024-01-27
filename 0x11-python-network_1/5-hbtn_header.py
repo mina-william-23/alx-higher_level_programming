d490b23dd530f74b66cbc3bbed357230a25606cb
mina-william-23 <capeandroid50@gmail.com>
2024-01-27 04:59:56 +0200

    bulk update

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     import sys
     url = sys.argv[1]
     req = requests.get(url)
-    print(r.headers.get('X-Request-Id'))
+    print(req.headers.get('X-Request-Id'))
     # print(req.headers['X-Request-Id'])
