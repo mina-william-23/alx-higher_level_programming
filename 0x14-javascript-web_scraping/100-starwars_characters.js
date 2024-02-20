f90459d21305bf27b46b0f5ebe87678081d69000
mina-william-23 <capeandroid50@gmail.com>
2024-02-20 23:49:38 +0200

    task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-const fs = require('fs');
 const id = process.argv[2];
 const link = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request(link, (error, response, body) => {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    for (const character_link of characters) {
-      request(character_link, (error, response, body) => {
+    for (const characterLink of characters) {
+      request(characterLink, (error, response, body) => {
         if (!error) {
           console.log(JSON.parse(body).name);
         }
